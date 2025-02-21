0ad974c531932aea653616cb1981af6392b63080
nazim uddin <mdnajim1247@gmail.com>
2025-02-21 19:31:58 +0600

    updated code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9njqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,11 +53,58 @@ async function run() {
         res.send(result);
     })
 
+    // app.post('/tasks', (req, res) => {
+    //     const { title, description, category } = req.body;
+    
+    //     // Input Validation
+    //     if (!title || title.length > 50) {
+    //         return res.status(400).json({ error: "Title is required and must be within 50 characters." });
+    //     }
+    //     if (description && description.length > 200) {
+    //         return res.status(400).json({ error: "Description must be within 200 characters." });
+    //     }
+    
+    //     // Create New Task
+    //     const newTask = {
+    //         id: tasks.length + 1, // Simple unique ID
+    //         title,
+    //         description: description || "",
+    //         timestamp: new Date().toISOString(),
+    //         category: category || "To-Do",
+    //     };
+    
+    //     tasks.push(newTask);
+    //     res.status(201).json(newTask);
+    // });
+
     // get all tasks
     app.get('/tasks', async(req,res)=>{
         const result = await taskCollection.find().toArray();
         res.send(result);
     })
+
+    // update task
+    app.put("/tasks/:id", async (req, res) => {
+        const { id } = req.params;
+        const { category } = req.body;
+    
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+            $set: { category: category }
+        };
+    
+        const result = await taskCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    });
+
+    // get all Task a specific user
+    app.get('/tasks/:email',async(req,res)=>{
+        const email = req.params.email;
+        const query = {agentEmail: email};
+        const result = await taskCollection.find(query).toArray();
+        res.send(result);
+    })
+
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
